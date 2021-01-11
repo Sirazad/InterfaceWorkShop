f168b23da7086eb75ef7a9bd8b5c9cdc4cf6fa40
Sirazad <lajosimola2@gmail.com>
2021-01-11 09:48:29 +0100

    base

---
+++ src/main/java/com/codecool/interfaceWS/Bird.java
@@ -0,0 +1,12 @@
+package com.codecool.interfaceWS ;
+
+public class Bird {
+
+    private String name;
+    private boolean isMale;
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/codecool/interfaceWS/LadyBird.java
@@ -0,0 +1,9 @@
+package com.codecool.interfaceWS;
+
+public class LadyBird {
+
+    private String name;
+    private boolean isMale;
+
+
+}
